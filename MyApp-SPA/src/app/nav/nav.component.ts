8aeacd1fd534ead583eee818a2e99aec947eac72
Vandesh <Kadgevandesh@gmail.com>
2020-07-26 16:13:20 +0530

    Added Routing

--- MyApp-SPA/src/app/nav/nav.component.ts
+++ MyApp-SPA/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import {AlertifyService} from '../_services/alertify.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,7 +10,9 @@ import {AlertifyService} from '../_services/alertify.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService,
+     private alertify: AlertifyService,
+      private router: Router) { }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
@@ -22,7 +24,9 @@ export class NavComponent implements OnInit {
     },
     error => {
       this.alertify.error(error);
-    });
+    }, () => { this.router.navigate(['/members']);
+  });
+
 
   }
 
@@ -36,5 +40,6 @@ export class NavComponent implements OnInit {
   {
     localStorage.removeItem('token');
     this.alertify.success('Logged out sucessfully');
+    this.router.navigate(['/home']);
   }
 }
